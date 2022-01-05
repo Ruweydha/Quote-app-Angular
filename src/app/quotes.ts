6d7e9cd28946f83b6b1a6fab31923e07b19a1a64
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-05 23:26:00 +0300

    Created a class

---
+++ src/app/quotes.ts
@@ -0,0 +1,5 @@
+export class Quotes {
+    constructor(public id: number,public name: string,public quote: string, public author: string,public publishDate: Date, public upVote:number, public downVote:number){
+
+    }
+}

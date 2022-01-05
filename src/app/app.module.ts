86fd50eda447c99ad3234fb89d6774a639aec628
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-05 23:30:08 +0300

    Updated itself when I generate the components and directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { FormsModule } from '@angular/forms';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { VotesDirective } from './votes.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesFormComponent,
+    QuotesDetailsComponent,
+    QuotesComponent,
+    VotesDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

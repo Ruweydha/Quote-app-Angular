1d3cbd984266aad36143bfefa72c149f786c8be4
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-07 15:00:12 +0300

    Added a show details property

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,5 +1,7 @@
 export class Quotes {
+    showDetails:boolean
     constructor(public id: number,public name: string,public quote: string, public author: string,public publishDate: Date, public upVote:number, public downVote:number){
+        this.showDetails = false;
 
     }
 }

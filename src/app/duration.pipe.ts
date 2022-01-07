9993a8e78c4a59f95c35f6c019e7cf8f7aa0ce87
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-07 14:51:26 +0300

    Wrote the code for the custom pipe

--- src/app/duration.pipe.ts
+++ src/app/duration.pipe.ts
@@ -5,8 +5,42 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DurationPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let time:any = "";
+    let today:any = new Date();
+    const secondsInDay = 86400;
+    const secondsInHour = 3600;
+    var timeDifference = Math.abs(value - today)/1000;
+    var numberOfDays = Math.floor(timeDifference/secondsInDay);
+
+    if(numberOfDays >0){
+      time += `${Math.floor(numberOfDays)}Days `;
+
+    }
+    timeDifference -= numberOfDays * secondsInDay;
+    var numberOfHours = Math.floor(timeDifference/secondsInHour);
+
+   if(numberOfHours > 0){
+      time += `${Math.floor(numberOfHours)}Hours `
+    }
+    timeDifference -= numberOfHours * secondsInHour;
+    var minutes = Math.floor(timeDifference/60);
+
+    if(minutes > 0){
+      time += `${Math.floor(minutes)}Minutes `
+    }
+    timeDifference -= minutes *60;
+    var seconds = timeDifference %60;
+
+   if(seconds > 0){
+      time += `${Math.floor(seconds)}Seconds `
+    }
+
+      return time;
+     
+   
+     
+
   }
 
 }
